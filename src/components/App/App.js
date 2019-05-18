9277d27e79473d5a6bdcf43373f91a9d08bde7f2
Stefen Menzel <stuff@Stefens-MBP.lan>
2019-05-17 22:28:01 -0500

    started setting up frontend requests and made a couple of blank components for later

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Header from '../Header/Header.js';
+import FeelyImageCarousel from '../FeelyImageCarousel/FeelyImageCarousel.js';
+
 class App extends Component {
   // Renders the entire app on the DOM
   render() {
     return (
       <div className="App">
-        <p>Empty Page</p>
+        <Header />
+        <FeelyImageCarousel />
       </div>
     );
   }
