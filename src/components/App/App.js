d8dea38c8c9c799e2ff09ff84fc223bdb0a16878
Stefen Menzel <stuff@Stefens-MBP.lan>
2019-05-17 23:07:51 -0500

    got the images cycling through, back and forth, looping around...to and fro

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import {connect} from 'react-redux';
 
 import Header from '../Header/Header.js';
 import FeelyImageCarousel from '../FeelyImageCarousel/FeelyImageCarousel.js';
 
 class App extends Component {
+
+  componentWillMount(){
+    this.props.dispatch({type: 'FETCH_IMAGES'});
+    this.props.dispatch({type: 'FETCH_TAGS'});
+  }
+
   // Renders the entire app on the DOM
   render() {
     return (
@@ -16,4 +23,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect()(App);
--- src/components/FeelyImageCarousel/FeelyImageCarousel.js
+++ src/components/FeelyImageCarousel/FeelyImageCarousel.js
@@ -3,17 +3,67 @@ import {connect} from 'react-redux';
 
 class FeelyImageCarousel extends Component{
 
+    state = {
+        currentImageIndex: 0
+    }
+
+    // componentDidMount(){
+    //     this.props.dispatch({type:'FETCH_IMAGES'});
+    // }
+
+    renderImage = () => {
+        return(
+            (this.props.images.length > 0) ?
+            <img src={this.props.images[this.state.currentImageIndex].path} alt=''/> :
+            <div></div>
+        )
+    }
+
+    adjustIndex = (incrementOrDecrement) => {
+        if(incrementOrDecrement === 'increase'){
+            if(this.state.currentImageIndex+1 > this.props.images.length-1)
+            this.setState({
+                currentImageIndex: 0
+            });
+            else{
+                this.setState({
+                    currentImageIndex: this.state.currentImageIndex+1
+                })
+            }
+        }
+        else{
+            if(this.state.currentImageIndex-1 < 0){
+                this.setState({
+                    currentImageIndex: this.props.images.length-1
+                });
+            }
+            else{
+                this.setState({
+                    currentImageIndex: this.state.currentImageIndex - 1
+                });
+            }            
+        }
+    }
+
     render(){
+        console.log("image 0 path is:", this.props.images[0]);
         return(
             <div>
-                <h2>Blah Blah Blah Feely Image Carousel</h2>
+                <pre>{JSON.stringify(this.props.images[0])}</pre>
+                <button onClick={() => this.adjustIndex('decrease')}>Previous</button>
+                {this.renderImage()}
+                <button onClick={() => this.adjustIndex('increase')}>Next</button>
+                {/* <img src={this.props.images[this.state.currentImageIndex].path} alt=''/> */}
             </div>
         )
     }
 }
 
 const mapStateToProps = (reduxState) => {
-    return { reduxState};
-}
+    return {
+        images: reduxState.images,
+        tags: reduxState.tags
+    };
+};
 
-export default connect()(FeelyImageCarousel)
+export default connect(mapStateToProps)(FeelyImageCarousel)
